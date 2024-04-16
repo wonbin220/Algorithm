a262ee0f4ef6b611808d3755522986ec26f0a06a
wonbin220 <wonbin220@gmail.com>
2024-04-16 22:35:12 +0900

    제로베이스 선형자료구조 - 스택

---
+++ linearDS/stackPractice2.java
@@ -0,0 +1,50 @@
+package linearDS_06Practice;
+//괄호 짝 검사
+
+// 입출력 예시
+// 입력 : "("
+// 출력 : Fail
+
+// 입력 : ")"
+// 출력 : Fail
+
+// 입력 : "()"
+// 출력 : Pass
+
+
+import java.util.Stack;
+
+public class Practice2 {
+    public static void checkParentthesis(String str) {
+        Stack stack = new Stack();
+        boolean checkFlag = true;//괄호짝이 맞는지 안맞는지
+
+        for (String s : str.split("")) {
+            if (s.equals("(")) {
+                stack.push(s);
+            } else {
+                if (stack.isEmpty()) {
+                    checkFlag = false;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if (checkFlag && stack.isEmpty()) {
+            System.out.println("PASS!");
+        } else {
+            System.out.println("FAIL!");
+        }
+    }
+
+    public static void main(String[] args) {
+    //  Test code
+        checkParentthesis("(");             // FAIL!
+        checkParentthesis("(");             // FAIL!
+        checkParentthesis("()");            // PASS!
+        checkParentthesis("()()()");        // PASS!
+        checkParentthesis("(())()");        // PASS!
+        checkParentthesis("(((()))");       // FAIL!
+    }
+}
---
+++ linearDS/stackPractice3.java
@@ -0,0 +1,50 @@
+package linearDS_06Practice;
+
+// 후위표기법 연산
+// 참고 설명) 전위/중위/후위 표기법
+
+// 입출력 예시
+// 입력 : " 2 2 +"
+// 출력 : 4
+
+// 입력 : "2 2 -"
+// 출력 : 0
+
+import java.util.Stack;
+
+public class Practice3 {
+    public static double calculate(String str) {
+        // Stack stack = new Stack();
+        Stack<Double> stack = new Stack();
+
+        for (String s : str.split(" ")) {
+            if (s.equals("+")) {
+                // stack.pop()((double)stack.pop() + (double) stack.pop());
+                // 형변환을 계속 쓰기 힘들면 Stack<double> stack로 만들면된다.
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                stack.push(- stack.pop() + stack.pop());
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                stack.push(1 / stack.pop() * stack.pop());
+            } else {
+                stack.push(Double.parseDouble(s)); // 한 글자들은 String타입이기때문에
+                                                    //Double형으로 바꿔서
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+    //  Test code
+        System.out.println(calculate("2 2 +"));         // 4
+        System.out.println(calculate("2 2 -"));         // 0
+        System.out.println(calculate("2 2 *"));         // 4
+        System.out.println(calculate("2 2 /"));         // 1
+
+
+        System.out.println(calculate("1 1 + 2 * 3 * 2 / 5 -"));     // 1
+        System.out.println(calculate("5 2 * 3 - 8 * 4 /"));         // 14
+    }
+}
