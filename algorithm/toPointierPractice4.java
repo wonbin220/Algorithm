98085c2f58fef3a19982f8d18054f15b0d2ba0d4
wonbin220 <wonbin220@gmail.com>
2024-05-02 18:26:36 +0900

    제로베이스 알고리즘 - 투 포인터 문제 풀이

---
+++ algorithm/toPointierPractice4.java
@@ -0,0 +1,61 @@
+package Algorithm_06_Practices;// Practice
+// 주어진 nums 배열에서 3 개의 합이 0이 되는 모든 숫자들을 출력하세요.
+// 중복된 숫자 셋은 제외하고 출력하세요.
+
+// 입출력 예시
+// nums: {-1, 0, 1, 2, -1, -4};
+// 출력: [[-1, -1, 2], [-1, 0, 1]]
+
+// nums: {1, -7, 6, 3, 5, 2}
+// 출력: [[-7, 1, 6], [-7, 2, 5]]
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Practice4 {
+    public static ArrayList<ArrayList<Integer>> solution(int[] nums) {
+        Arrays.sort(nums);
+        ArrayList<ArrayList<Integer>> result =  new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
+                int p1 = i + 1;
+                int p2 = nums.length - 1;
+                int sum = 0 - nums[i];
+
+                while (p1 < p2) {
+                    if (nums[p1] + nums[p2] == sum) {
+                        result.add(new ArrayList<>(Arrays.asList(nums[i], nums[p1], nums[p2])));
+
+                        while (p1 < p2 && nums[p1] == nums[p1 + 1]) {
+                            p1++;
+                        }
+
+                        while (p1 < p2 && nums[p2] == nums[p2 - 1]) {
+                            p2--;
+                        }
+
+                        p1++;
+                        p2--;
+                    } else if (nums[p1] + nums[p2] < sum) {
+                        p1++;
+                    } else {
+                        p2--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        int[] nums = {-1, 0, 1, 2, -1, -4};     // [[-1, -1, 2], [-1, 0, 1]]
+        System.out.println(solution(nums));
+
+        nums = new int[] {1, -7, 6, 3, 5, 2};   // [[-7, 1, 6], [-7, 2, 5]]
+        System.out.println(solution(nums));
+    }
+}
